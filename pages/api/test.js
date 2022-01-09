b50c32c4bfa3fa697ae42518284553c36386086c
Andrew Kerr <andrewkerr3956@gmail.com>
2022-01-09 18:29:53 -0500

    Another production build test.

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,3 +1,8 @@
+const mysql = require('./../../lib/db');
 export default function handler(req, res) {
-    res.send({host: process.env.MYSQL_HOST})    
+    let testthis = mysql.pool.connect((err, conn) => {
+        if(err) throw err;
+        testthis = conn;
+    })
+    res.send({host: process.env.MYSQL_HOST, sql: })    
 }
