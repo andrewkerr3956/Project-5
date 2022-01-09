6ea98f031a624bdfe4fb1652e2ce10808d59e56d
Andrew Kerr <andrewkerr3956@gmail.com>
2022-01-09 18:24:44 -0500

    Re-commiting to try to test some issues that the production build is having.

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,3 @@
-const mysql = require('./../../lib/db');
-
 export default function handler(req, res) {
-    mysql.pool.getConnection((err, conn) => {
-        res.status(200).send({success: "We are in the connection!"});
-        mysql.pool.releaseConnection(conn);
-    });
+    res.send({host: process.env.MYSQL_HOST})    
 }
